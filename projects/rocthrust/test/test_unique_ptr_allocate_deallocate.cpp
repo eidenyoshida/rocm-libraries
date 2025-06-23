1c8f941cdd82bfdc10dddba4e0329cca75f96fc3
Marko Savic <marsavic@amd.com>
2025-06-23 09:55:29 -0400

    Add make_unique tests

--- projects/rocthrust/test/test_unique_ptr_allocate_deallocate.cpp
+++ projects/rocthrust/test/test_unique_ptr_allocate_deallocate.cpp
@@ -3,3 +3,43 @@
 #include "hip/hip_runtime.h"
 #include "test_param_fixtures.hpp"
 #include "test_utils.hpp"
+
+class A {
+public:    
+    __host__ __device__ A() {}
+    __host__ __device__ A(int num): val_a(num) {}
+
+    __host__ __device__ ~A() {}
+ 
+    int val_a;
+}; 
+
+TESTS_DEFINE(UniquePtrAllocDeallocTests, NumericalTestsParams);
+
+TYPED_TEST(UniquePtrAllocDeallocTests, TestMakeUnique)
+{
+    using T = typename TestFixture::input_type;
+    SCOPED_TRACE(testing::Message() << "with device_id= " << test::set_device_from_ctest());
+
+    T initial_value = T(1);
+    if(std::is_floating_point<T>::value)
+    {
+        initial_value = T(7.71);
+    }
+
+    thrust::unique_ptr<T> p1 = thrust::make_unique<T>(initial_value);
+    ASSERT_EQ(*p1, initial_value);
+
+    p1 = thrust::make_unique<T>();
+    ASSERT_EQ(*p1, T {});
+}
+
+TEST(UniquePtrAllocDeallocTests, TestMakeUniqueUserType)
+{
+    SCOPED_TRACE(testing::Message() << "with device_id= " << test::set_device_from_ctest());
+
+    thrust::unique_ptr<A> p = thrust::make_unique<A>(7);
+    A host_p = *p;
+
+    ASSERT_EQ(host_p.val_a, 7);
+}
