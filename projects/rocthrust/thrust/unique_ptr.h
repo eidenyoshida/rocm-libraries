5a418c05c553bc1fbd14aa5b7bf728b28d38815c
Marko Savic <marsavic@amd.com>
2025-07-29 05:35:16 -0400

    Add ctor with raw pointer for both up forms

--- projects/rocthrust/thrust/unique_ptr.h
+++ projects/rocthrust/thrust/unique_ptr.h
@@ -231,6 +231,12 @@ public:
       , m_deleter()
   {}
 
+  template <bool Dummy = true, class = EnableIfDeleterDefaultConstructible<Dummy>>
+  THRUST_HOST THRUST_CONSTEXPR_SINCE_CXX23 explicit unique_ptr(T* raw_p) noexcept
+      : m_ptr(device_pointer_cast(raw_p))
+      , m_deleter()
+  {}
+
   template <bool Dummy = true, class = EnableIfDeleterConstructible<LValRefType<Dummy>>>
   THRUST_HOST THRUST_CONSTEXPR_SINCE_CXX23 unique_ptr(pointer p, LValRefType<Dummy> d) noexcept
       : m_ptr(p)
@@ -302,7 +308,7 @@ public:
   template <bool Dummy = true, class = EnableIfDeleterDefaultDelete<Dummy>>
   THRUST_HOST THRUST_CONSTEXPR_SINCE_CXX23 T* get_raw() const noexcept
   {
-    return thrust::raw_pointer_cast(m_ptr);
+    return raw_pointer_cast(m_ptr);
   }
 
   THRUST_HOST THRUST_CONSTEXPR_SINCE_CXX23 deleter_type& get_deleter() noexcept
@@ -458,12 +464,29 @@ public:
       , m_deleter()
   {}
 
+  template <bool Dummy = true,
+            class      = EnableIfDeleterDefaultConstructible<Dummy>,
+            class      = EnableIfPointerConvertible<device_ptr<T>>,
+            class      = EnableIfTriviallyDestructible<Dummy>>
+  THRUST_HOST THRUST_CONSTEXPR_SINCE_CXX23 explicit unique_ptr(T* raw_p) noexcept
+      : m_ptr(device_pointer_cast(raw_p))
+      , m_deleter()
+  {}
+
   template <class Pp,
             bool Dummy = true,
             class      = EnableIfDeleterDefaultConstructible<Dummy>,
             class      = EnableIfPointerConvertible<Pp>>
   THRUST_HOST THRUST_CONSTEXPR_SINCE_CXX23 explicit unique_ptr(Pp p, size_t size) noexcept
       : m_ptr(p)
+      , m_deleter(size)
+  {}
+
+  template <bool Dummy = true,
+            class      = EnableIfDeleterDefaultConstructible<Dummy>,
+            class      = EnableIfPointerConvertible<device_ptr<T>>>
+  THRUST_HOST THRUST_CONSTEXPR_SINCE_CXX23 explicit unique_ptr(T* raw_p, size_t size) noexcept
+      : m_ptr(device_pointer_cast(raw_p))
       , m_deleter(size)
   {}
 
@@ -574,7 +597,7 @@ public:
 
   THRUST_HOST THRUST_CONSTEXPR_SINCE_CXX23 T* get_raw() const noexcept
   {
-    return thrust::raw_pointer_cast(m_ptr);
+    return raw_pointer_cast(m_ptr);
   }
 
   THRUST_HOST THRUST_CONSTEXPR_SINCE_CXX23 deleter_type& get_deleter() noexcept
