63cbeaa6e7ac5c96398fb474b715bc9a296c883c
Marko Savic <marsavic@amd.com>
2025-08-15 18:57:36 -0400

    Add test for move assignment

--- projects/rocthrust/test/test_unique_ptr_general.cpp
+++ projects/rocthrust/test/test_unique_ptr_general.cpp
@@ -24,3 +24,44 @@ TEST(UniquePtrGeneralTests, TestUniquePtrSwap)
         ASSERT_EQ(p2.get_raw(), raw_p1);
     }
 }
+
+TEST(UniquePtrGeneralTests, TestUniquePtrMoveAsgn)
+{
+    SCOPED_TRACE(testing::Message() << "with device_id= " << test::set_device_from_ctest());
+
+    // Move assignment for single objects
+    {
+        void* raw_addr = nullptr;
+
+        thrust::unique_ptr<int> p1 = thrust::make_unique<int>(1);
+        int* raw_p1 = p1.get_raw();
+        {
+            thrust::unique_ptr<int> p2 = thrust::make_unique<int>(2);
+            ASSERT_NE(p1.get_raw(), nullptr);
+            ASSERT_NE(p2.get_raw(), nullptr);
+
+            p2 = std::move(p1);
+
+            ASSERT_EQ(p2.get_raw(), raw_p1);
+            ASSERT_EQ(p1.get_raw(), nullptr);
+
+            raw_addr = static_cast<void*>(p2.get_raw());
+        }
+        size_t dummy = 0;
+        hipError_t st = hipMemPtrGetInfo(raw_addr, &dummy);
+        ASSERT_EQ(st, hipErrorInvalidValue);
+    }  
+}
+
+TEST(UniquePtrGeneralTests, TestUnqiuePtrSelfMoveAsgn)
+{
+    SCOPED_TRACE(testing::Message() << "with device_id= " << test::set_device_from_ctest());
+
+    // Self-move for single object
+    {   
+        thrust::unique_ptr<int> p = thrust::make_unique<int>(1);
+        int* raw_p = p.get_raw();
+        p = std::move(p);
+        ASSERT_EQ(p.get_raw(), raw_p);
+    }
+}
