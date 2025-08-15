64280c2bea2b244b000eb4a378f8685cb739e7c7
Marko Savic <marsavic@amd.com>
2025-08-15 18:43:08 -0400

    Add test for swap array

--- projects/rocthrust/test/test_unique_ptr_general.cpp
+++ projects/rocthrust/test/test_unique_ptr_general.cpp
@@ -76,6 +76,24 @@ TEST(UniquePtrGeneralTests, TestUniquePtrSwap)
     }
 }
 
+TEST(UniquePtrGeneralTests, TestUniquePtrSwapArray)
+{
+    SCOPED_TRACE(testing::Message() << "with device_id= " << test::set_device_from_ctest());
+
+    // Test swap for arrays
+    {
+        thrust::unique_ptr<int[]> p1 = thrust::make_unique<int[]>(5);
+        thrust::unique_ptr<int[]> p2 = thrust::make_unique<int[]>(10);
+
+        int* raw_p1 = p1.get_raw();
+        int* raw_p2 = p2.get_raw();
+
+        p1.swap(p2);
+        ASSERT_EQ(p1.get_raw(), raw_p2);
+        ASSERT_EQ(p2.get_raw(), raw_p1);
+    }
+}
+
 TEST(UniquePtrGeneralTests, TestUniquePtrMoveAsgn)
 {
     SCOPED_TRACE(testing::Message() << "with device_id= " << test::set_device_from_ctest());
