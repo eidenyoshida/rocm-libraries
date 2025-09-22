cd78d76a4924e6c8f9ae446d3979bc5a8e1442d7
Robin Voetter <robin@streamhpc.com>
2025-09-22 10:49:47 +0200

    pooling: forward naive initial port

--- projects/miopen/src/kernels/MIOpenPoolingForwardNaive.cpp
+++ projects/miopen/src/kernels/MIOpenPoolingForwardNaive.cpp
@@ -2,7 +2,7 @@
  *
  * MIT License
  *
- * Copyright (c) 2023 Advanced Micro Devices, Inc.
+ * Copyright (c) 2025 Advanced Micro Devices, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -24,9 +24,14 @@
  *
  *******************************************************************************/
 
+#ifndef MIOPEN_DONT_USE_HIP_RUNTIME_HEADERS
+#include <hip/hip_runtime.h>
+#endif
+
+#include "miopen_limits.hpp"
 #include "pooling_functions.h"
 
-#if(MLO_POOLING_OP_ID == MLO_POOLING_OP_AVE) || (MLO_POOLING_OP_ID == MLO_POOLING_OP_AVE_INCLUSIVE)
+#if (MLO_POOLING_OP_ID == MLO_POOLING_OP_AVE) || (MLO_POOLING_OP_ID == MLO_POOLING_OP_AVE_INCLUSIVE)
 #define AVERAGE_OPS 1
 #else
 #define AVERAGE_OPS 0
@@ -37,119 +42,108 @@
 #if !(AVERAGE_OPS && MIOPEN_USE_FP16)
 #define MIOPEN_USE_NATIVE_DATATYPE_ACCUM 1
 #endif
+
 #include "float_types.h"
 
 #ifndef MLO_POOLING_IS2D_KERNEL
 #error "MLO_POOLING_IS2D_KERNEL must be defined"
 #endif
 
-#if AVERAGE_OPS
-#define ARG_UNUSED_FOR_AVERAGE __attribute__((__unused__))
-#else
-#define ARG_UNUSED_FOR_AVERAGE
-#endif
+using arg_size_t = unsigned long;
 
-#if MLO_POOLING_IS2D_KERNEL
-#define ARG_UNUSED_FOR_2D __attribute__((__unused__))
-#else
-#define ARG_UNUSED_FOR_2D
-#endif
-
-typedef unsigned long arg_size_t;
-
-__kernel void mloPoolingForwardNaive(const __global _FLOAT* bot_ptr,
-                                     __global _FLOAT* top_ptr,
-                                     ARG_UNUSED_FOR_AVERAGE __global index_t* mask_ptr,
-                                     ARG_UNUSED_FOR_AVERAGE int save_index,
-                                     ARG_UNUSED_FOR_AVERAGE int index_mode,
-                                     uint filter_d,
-                                     uint filter_h,
-                                     uint filter_w,
-                                     uint filter_d_stride,
-                                     uint filter_h_stride,
-                                     uint filter_w_stride,
-                                     uint filter_d_pad,
-                                     uint filter_h_pad,
-                                     uint filter_w_pad,
-                                     uint all_n,
-                                     uint all_c,
-                                     uint bot_d,
-                                     uint bot_h,
-                                     uint bot_w,
-                                     arg_size_t bot_n_stride,
-                                     arg_size_t bot_c_stride,
-                                     uint bot_d_stride,
-                                     uint bot_h_stride,
-                                     uint bot_w_stride,
-                                     ARG_UNUSED_FOR_2D uint top_d,
-                                     uint top_h,
-                                     uint top_w,
-                                     arg_size_t top_n_stride,
-                                     arg_size_t top_c_stride,
-                                     uint top_d_stride,
-                                     uint top_h_stride,
-                                     uint top_w_stride,
-                                     ARG_UNUSED_FOR_AVERAGE arg_size_t mask_n_stride,
-                                     ARG_UNUSED_FOR_AVERAGE arg_size_t mask_c_stride,
-                                     ARG_UNUSED_FOR_AVERAGE uint mask_d_stride,
-                                     ARG_UNUSED_FOR_AVERAGE uint mask_h_stride,
-                                     ARG_UNUSED_FOR_AVERAGE uint mask_w_stride)
+extern "C" __global__ void mloPoolingForwardNaive(const FLOAT* bot_ptr,
+                                                  FLOAT* top_ptr,
+                                                  [[maybe_unused]] index_t* mask_ptr,
+                                                  [[maybe_unused]] int save_index,
+                                                  [[maybe_unused]] int index_mode,
+                                                  unsigned int filter_d,
+                                                  unsigned int filter_h,
+                                                  unsigned int filter_w,
+                                                  unsigned int filter_d_stride,
+                                                  unsigned int filter_h_stride,
+                                                  unsigned int filter_w_stride,
+                                                  unsigned int filter_d_pad,
+                                                  unsigned int filter_h_pad,
+                                                  unsigned int filter_w_pad,
+                                                  unsigned int all_n,
+                                                  unsigned int all_c,
+                                                  unsigned int bot_d,
+                                                  unsigned int bot_h,
+                                                  unsigned int bot_w,
+                                                  arg_size_t bot_n_stride,
+                                                  arg_size_t bot_c_stride,
+                                                  unsigned int bot_d_stride,
+                                                  unsigned int bot_h_stride,
+                                                  unsigned int bot_w_stride,
+                                                  [[maybe_unused]] unsigned int top_d,
+                                                  unsigned int top_h,
+                                                  unsigned int top_w,
+                                                  arg_size_t top_n_stride,
+                                                  arg_size_t top_c_stride,
+                                                  unsigned int top_d_stride,
+                                                  unsigned int top_h_stride,
+                                                  unsigned int top_w_stride,
+                                                  [[maybe_unused]] arg_size_t mask_n_stride,
+                                                  [[maybe_unused]] arg_size_t mask_c_stride,
+                                                  [[maybe_unused]] unsigned int mask_d_stride,
+                                                  [[maybe_unused]] unsigned int mask_h_stride,
+                                                  [[maybe_unused]] unsigned int mask_w_stride)
 {
-    const uint b = get_global_id(0);
+    const unsigned int b = blockIdx.x * blockDim.x + threadIdx.x;
     if(!(b < all_n))
         return;
 
-    const uint o = get_global_id(1);
+    const unsigned int o = blockIdx.y * blockDim.y + threadIdx.y;
     if(!(o < all_c))
         return;
 
 #if MLO_POOLING_IS2D_KERNEL
     // When we want 2D kernel, run only inner loop.
     // Fix k to 0 and take current j from the grid.
-    const uint k = 0; // top_d == 1
-    const uint j = get_global_id(2);
+    const unsigned int k = 0; // top_d == 1
+    const unsigned int j = blockIdx.z * blockDim.z + threadIdx.z;
     if(!(j < top_h))
         return;
 #else
-    const uint k = get_global_id(2);
+    const unsigned int k = blockIdx.z * blockDim.z + threadIdx.z;
     if(!(k < top_d))
         return;
-    for(uint j = 0; j < top_h; ++j)
+    for(unsigned int j = 0; j < top_h; ++j)
     {
 #endif
-    for(uint i = 0; i < top_w; ++i)
+    for(unsigned int i = 0; i < top_w; ++i)
     {
-        const int int_dstart = k * filter_d_stride - filter_d_pad;
-        const int int_hstart = j * filter_h_stride - filter_h_pad;
-        const int int_wstart = i * filter_w_stride - filter_w_pad;
-        const uint dend      = (uint)min(int_dstart + (int)filter_d, (int)bot_d);
-        const uint hend      = (uint)min(int_hstart + (int)filter_h, (int)bot_h);
-        const uint wend      = (uint)min(int_wstart + (int)filter_w, (int)bot_w);
-        const uint dstart    = (uint)max(int_dstart, 0);
-        const uint hstart    = (uint)max(int_hstart, 0);
-        const uint wstart    = (uint)max(int_wstart, 0);
+        const int int_dstart      = k * filter_d_stride - filter_d_pad;
+        const int int_hstart      = j * filter_h_stride - filter_h_pad;
+        const int int_wstart      = i * filter_w_stride - filter_w_pad;
+        const unsigned int dend   = (unsigned int)min(int_dstart + (int)filter_d, (int)bot_d);
+        const unsigned int hend   = (unsigned int)min(int_hstart + (int)filter_h, (int)bot_h);
+        const unsigned int wend   = (unsigned int)min(int_wstart + (int)filter_w, (int)bot_w);
+        const unsigned int dstart = (unsigned int)max(int_dstart, 0);
+        const unsigned int hstart = (unsigned int)max(int_hstart, 0);
+        const unsigned int wstart = (unsigned int)max(int_wstart, 0);
 
 #if MLO_POOLING_OP_ID == MLO_POOLING_OP_AVE
-        uint pool_size = (dend - dstart) * (hend - hstart) * (wend - wstart);
-        pool_size      = (pool_size == 0) ? 1 : pool_size;
+        unsigned int pool_size = (dend - dstart) * (hend - hstart) * (wend - wstart);
+        pool_size              = (pool_size == 0) ? 1 : pool_size;
 #elif MLO_POOLING_OP_ID == MLO_POOLING_OP_AVE_INCLUSIVE
-            const uint pool_size = filter_w * filter_h * filter_d;
+            const unsigned int pool_size = filter_w * filter_h * filter_d;
 #endif
 
 #if AVERAGE_OPS
-        _FLOAT_ACCUM res = (_FLOAT_ACCUM)(0);
+        FLOAT_ACCUM res = (FLOAT_ACCUM)(0);
 #else // MAX
-            _FLOAT_ACCUM res     = (_FLOAT_ACCUM)(-MAX_VAL_ACCUM);
-            bool found           = false; // May remain false if bot contains only NaNs/-INFs.
-            uint d_save          = 0;
-            uint h_save          = 0;
-            uint w_save          = 0;
+            FLOAT_ACCUM res     = (FLOAT_ACCUM)(-MAX_VAL_ACCUM);
+            bool found          = false; // May remain false if bot contains only NaNs/-INFs.
+            unsigned int d_save = 0;
+            unsigned int h_save = 0;
+            unsigned int w_save = 0;
 #endif
-        for(uint d = dstart; d < dend; ++d)
+        for(unsigned int d = dstart; d < dend; ++d)
         {
-            for(uint h = hstart; h < hend; ++h)
+            for(unsigned int h = hstart; h < hend; ++h)
             {
-                for(uint w = wstart; w < wend; ++w)
+                for(unsigned int w = wstart; w < wend; ++w)
                 {
                     const size_t bot_index = b * bot_n_stride             //
                                              + o * bot_c_stride           //
@@ -176,14 +170,14 @@ __kernel void mloPoolingForwardNaive(const __global _FLOAT* bot_ptr,
         }
 
 #if AVERAGE_OPS
-        res *= CVT_FP32_2ACCUM(1.f) / (_FLOAT_ACCUM)pool_size;
+        res *= CVT_FP32_2ACCUM(1.f) / (FLOAT_ACCUM)pool_size;
 #else // MAX
             if(save_index)
             {
                 index_t res_index = 0;
 
                 /// Preventing overflow during computation of res_index:
-                /// If Index is shorter than uint, then let's perform computation in 32-bit
+                /// If Index is shorter than unsigned int, then let's perform computation in 32-bit
                 /// domain and then convert to narrower Index. That would reduce the probability of
                 /// overflow. If Index is wider then 32 bits, then it seems like it is better to
                 /// convert to Index type before multiplication. However this is not actually
@@ -218,7 +212,7 @@ __kernel void mloPoolingForwardNaive(const __global _FLOAT* bot_ptr,
                                  + (size_t)(j * top_h_stride) //
                                  + (size_t)(i * top_w_stride);
 
-        top_ptr[top_index] = (_FLOAT)res;
+        top_ptr[top_index] = (FLOAT)res;
     }
 #if !MLO_POOLING_IS2D_KERNEL
 }
