9dfdd418a10e79b0b01ddc795cbdd18b96fc3d95
Matthew Emmett <matthew.emmett@amd.com>
2025-09-25 08:21:30 -0600

    GEMM WIP

--- shared/rocroller/client/src/gemm.cpp
+++ shared/rocroller/client/src/gemm.cpp
@@ -1698,6 +1698,7 @@ int main(int argc, const char* argv[])
     problem.types  = types;
     solution.types = types;
 
+#if 0
     // Set default prefetchMixMemOps
     if(prefetchMixMemOpsFlag->count() == 0)
     {
@@ -1716,6 +1717,7 @@ int main(int argc, const char* argv[])
         if(solution.prefetchLDSFactor == 2 && (solution.direct2LDSA || solution.direct2LDSB))
             solution.prefetchMixMemOps = false;
     }
+#endif
 
     //
     // Run!
